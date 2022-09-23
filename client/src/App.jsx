6a6e5624aa1f9647d4d73262683d9350229ae083
jeison gaviria <jeison8725@gmail.com>
2022-09-23 10:49:10 -0500

    Proxy agregado

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div>
       <h1>Hello world</h1>
       <button onClick={async ()=>{
-          const response = await fetch('http://localhost:3000/users');
+          const response = await fetch('/users');
           const data = await response.json();
           console.log(data);
       }
